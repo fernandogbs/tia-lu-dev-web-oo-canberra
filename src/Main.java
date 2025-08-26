95ff1662893b0c183213b23c12a02a9f34ff7bbe
fernandogbs <httpfernandofg@gmail.com>
2025-08-26 16:27:29 -0300

    refactor: estrutura do MenuCLI e adiciona tratamento de erros

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Sistema de Restaurante");
+      try {
+        MenuCLI menu = new MenuCLI();
+        menu.iniciar();
+      } catch (Exception e) {
+        System.out.println("Error: " + e.getMessage());
+      }
     }
 }
